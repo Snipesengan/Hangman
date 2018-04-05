742d68f0e83856965007a5f3ae06d67b628509e1
Nhan Dao <nhan.dao@outlook.com>
2018-04-06 00:27:52 +0800

    Game now allows word guesses and work with window machines

--- Hangman.py
+++ Hangman.py
@@ -1,11 +1,11 @@
 import random
 import os
 
-def display(lives, hangman = [], alphabetSoup = [], guessString = []):
-    os.system('clear')  #For Linux/OS X
+def display(lives, hangman = [], missedGuess = [], guessString = []):
+    os.system('clear || cls')  #For Linux/OS X
     print("Lives: ", lives)
     print(''.join([char if (char == ' ' or char == '\n' or (int(char) < (10 - lives))) else ' ' for char in hangman])) #prints the hangman base on how many life is left.
-    print("Inccorect guess: " , ','.join([letter for letter in alphabetSoup if (not letter in word)]))
+    print("Inccorect guess: " , ','.join(missedGuess))
     print(' '.join(guessString))
 
 dictionary = open("dictionary.txt").read().split("\n")
@@ -29,20 +29,23 @@ hangman = list("""\
 
 word = dictionary[random.randrange(0, len(dictionary))]
 guessString = list("_" * len(word))
-alphabetSoup = [] #letters the user has guessed
+missedGuess = [] #letters the user has guessed
 lives = 10
 
-display(lives, hangman, alphabetSoup, guessString)
-while lives > 0 and '_' in guessString:
-    guessChar = input('Enter guess: ').lower()
 
-    if (guessChar.isalpha()) and (len(guessChar) == 1) and (guessChar.islower()) and (not guessChar in alphabetSoup):
-        alphabetSoup.append(guessChar)
-        if guessChar in word: #Correct Guess
-            guessString = [word[i] if word[i] == guessChar else char for i, char in enumerate(guessString)] #replaces the character in the guessString with correctly guessed letter
+display(lives, hangman, missedGuess, guessString)
+while lives > 0 and '_' in guessString:
+    guess = input('Enter guess: ').lower()
+
+    if (guess.isalpha()) and (not guess in missedGuess):
+        if guess == word: #Correct Guess
+            guessString = word
+        elif len(guess) == 1 and guess in word: #Correct Guess
+            guessString = [word[i] if word[i] == guess else char for i, char in enumerate(guessString)] #replaces the character in the guessString with correctly guessed letter
         else: #Incorrect Guess
+            missedGuess.append(guess)
             lives = lives - 1
 
-    display(lives, hangman, alphabetSoup, guessString)
+    display(lives, hangman, missedGuess, guessString)
 
 print("You won!" if lives > 0 else "You lost!\nCorrect word: {correctWord}".format(correctWord = word)) #Python is fucking ridiculous
